47505800c60f0877505f15a2ea1c9967a93ae0a4
Mario Cucurull Carcel <Mario Cucurull Carcel>
2017-04-01 21:05:34 +0200

    lel

---
+++ Fatalforma/Assets/Scripts/Pillars/FirePillarScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePillarScript : MonoBehaviour {
+
+    public GameObject target;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StaticData.firePillar = 1;
+            Destroy(gameObject);
+        }
+
+
+    }
+}
