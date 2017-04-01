bb9aa030b2cc335683298fb19139014a19ce402d
Daniel Ferreira <danferb@gmail.com>
2017-04-01 15:04:53 +0200

    Added BaseAssets from Mario40000
    Platforms and various code

---
+++ Fatalforma/Assets/Scripts/Scenario/DeathZone.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathZone : MonoBehaviour {
+
+	void OnTriggerEnter(Collider c){
+		//Application.LoadLevel(Application.loadedLevel);
+		int y = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadSceneAsync(y);
+	}
+}
