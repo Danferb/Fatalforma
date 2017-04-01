949049a660b542de1dadf57f8538b710f3149f9a
pedroolivagil <onion_oliva@hotmail.com>
2017-04-01 18:56:33 +0200

    platforms v6

--- Fatalforma/Assets/Scripts/Player/PlatformRotate.cs
+++ Fatalforma/Assets/Scripts/Player/PlatformRotate.cs
@@ -4,49 +4,35 @@ using UnityEngine;
 
 public class PlatformRotate : MonoBehaviour {
 
-    private float speed = 36;
-    private float delay = 5;
+    public float speed = 36;
+    public float delay = 5;
     private int change = 1;
-
-    
-
     //Update is called once per frame
-
-    void Start()
-    {
+    void Start() {
         StartCoroutine(Rotator());
     }
 
-    void FixedUpdate()
-    {
-        if (change < 1)
-        {
+    void FixedUpdate() {
+        if (change < 1) {
             transform.Rotate(Time.deltaTime * speed, 0, 0);
             StartCoroutine(StopRotation());
-        }
-        else
-        {
+        } else {
 
         }
 
 
     }
 
-    
-
-    IEnumerator Rotator ()
-    {
-        while (true)
-        {
+    IEnumerator Rotator() {
+        while (true) {
             change *= -1;
             yield return new WaitForSeconds(delay);
-        
+
         }
-        
+
     }
 
-    IEnumerator StopRotation ()
-    {
+    IEnumerator StopRotation() {
         yield return new WaitForSeconds(1);
         transform.Rotate(0, 0, 0);
     }
