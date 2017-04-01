67cf25c70ff521a09ec37c3d8bf69e596052d24d
Daniel Ferreira <danferb@gmail.com>
2017-04-01 15:54:38 +0200

    Better control

--- Fatalforma/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Fatalforma/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -12,10 +12,12 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private Vector3 m_CamForward;             // The current forward direction of the camera
         private Vector3 m_Move;
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+		public int turningSpeed;
 
         
         private void Start()
         {
+			turningSpeed = 2;
             // get the transform of the main camera
             if (Camera.main != null)
             {
@@ -48,6 +50,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             // read inputs
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
             float v = CrossPlatformInputManager.GetAxis("Vertical");
+			float mh = CrossPlatformInputManager.GetAxis("Mouse X");
+			float mv = CrossPlatformInputManager.GetAxis ("Mouse Y");
             bool crouch = Input.GetKey(KeyCode.C);
 
             // calculate move direction to pass to character
@@ -55,7 +59,12 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             {
                 // calculate camera relative direction to move:
                 m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = v*m_CamForward + h*m_Cam.right;
+				m_Move = v*m_CamForward + h*m_Cam.right+ mh*m_Cam.right*Time.deltaTime*turningSpeed;
+				//m_Cam.transform.Rotate(Vector3 (mv, mh, 0)*Time.deltaTime);
+				//transform.Rotate(Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime);
+				//Debug.Log(m_Cam.transform.right);
+				//Debug.Log (m_CamForward);
+
             }
             else
             {
